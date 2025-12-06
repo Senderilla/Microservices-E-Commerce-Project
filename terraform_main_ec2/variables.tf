4104ee931f13fc8089f5703e9f4367905cb8df03
Senderilla <senderillaesumu@gmail.com>
2025-12-06 12:46:43 +0100

    Update variables.tf

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -76,7 +76,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "Sendy-key pair(1).pem"
+  default     = "Sendy-key pair"
 }
 
 variable "instance_name" {
